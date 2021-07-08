45e945c41dddbeb124620b264d0ec4b2b8133d7a
”sovsianykov” <swebproger@gmail.com>
2021-07-08 18:02:02 +0300

    TabNavigation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import React, {useEffect, useState} from 'react';
-import { items, repHeaderItem, tabItems  } from "./components/ReportsNavigaton/mockContent/mock"
+import React, {useState} from 'react';
+import {items, repHeaderItem, tabItems} from "./components/ReportsNavigaton/mockContent/mock"
 import ReportsNavigation from "./components/ReportsNavigaton/ReportsNavigation";
 import RepHeader from "./components/RepHeader/RepHeader";
-import TabItem from "./components/TabNavigation/TabItem";
 import TabNavigation from "./components/TabNavigation/TabNavigation";
-
 
 
 function App() {
     const [itemsArr , setItemsArr] = useState(items)
     const [activeMenuItemId , setActiveMenuItemId] = useState('2')
-    const  header = repHeaderItem
-  return (
+    return (
     <div className="App">
       <ReportsNavigation
           items  = { itemsArr }
           activeMenuItemId = { activeMenuItemId }
       />
-        <RepHeader header={header}/>
+        <RepHeader header={ repHeaderItem }/>
      <TabNavigation items={ tabItems } activeTabItemId={'2'}/>
     </div>
   );
